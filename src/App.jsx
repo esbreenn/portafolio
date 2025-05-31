f125ceaf456b8e2dd02d264d0645df01bc7384b1
Gonzalo Fabricio Misael Romero Picon <programaciongonzalitox@gmail.com>
2025-05-31 08:06:26 -0300

    Fix error

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Importa tu layout principal
 import MainLayout from './layouts/MainLayout';
 
-// Importa tus componentes de página
+
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ProjectsPage from './pages/ProjectsPage';
-import ContactPage from './pages/ContactPage'; // Asumo que ConcatPage.jsx es un error tipográfico y es ContactPage.jsx
+import ContactPage from './pages/ContactPage'; 
 
-// Si tienes estilos específicos para App.jsx que no son globales, puedes importarlos aquí:
-// import './App.css';
+
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Define una ruta principal que usa tu MainLayout. */}
+   <Router basename="/portafolio/"> {/* <--- AÑADIDO basename */}      <Routes>
+        {/* Define una ruta principal que usa  MainLayout. */}
         {/* Las rutas anidadas se renderizarán dentro del <Outlet /> en MainLayout. */}
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} /> {/* Ruta para la página principal "/" */}
           <Route path="about" element={<AboutPage />} /> {/* Ruta para "/about" */}
           <Route path="projects" element={<ProjectsPage />} /> {/* Ruta para "/projects" */}
           <Route path="contact" element={<ContactPage />} /> {/* Ruta para "/contact" */}
-          {/* Puedes añadir más rutas aquí si creas más páginas */}
         </Route>
       </Routes>
     </Router>
