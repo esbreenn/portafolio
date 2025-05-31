bea5beecf12e3c5145e4b214936f013ccc627925
Gonzalo Fabricio Misael Romero Picon <programaciongonzalitox@gmail.com>
2025-05-31 07:58:43 -0300

    feat: Add GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' // Asegúrate de que esta importación esté allí
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),
-],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  base: '/portafolio/'
 })
